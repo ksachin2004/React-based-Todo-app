9f34b7260e8789f0a692c054d0b0f79566463dbb
ksachin2004 <sskhandekar2026@gmail.com>
2024-12-22 16:43:10 +0530

    ...

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleAddTodos = (newTodo) => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, newTodo]);
+    }
+  };
+
+  const handleDeleteTodo = (index) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
+
+  const handleEditTodo = (index) => {
+    const todoToEdit = todos[index];
+    setTodoValue(todoToEdit);
+    handleDeleteTodo(index); 
+  };
 
   return (
-    <>
-      <div> {4*4}</div>
-    </>
-  )
+    <main>
+      <TodoInput
+        handleAddTodos={handleAddTodos}
+        todoValue={todoValue}
+        setTodoValue={setTodoValue}
+      />
+      <TodoList
+        todos={todos}
+        handleDeleteTodo={handleDeleteTodo}
+        handleEditTodo={handleEditTodo}
+      />
+    </main>
+  );
 }
 
-export default App
+export default App;
